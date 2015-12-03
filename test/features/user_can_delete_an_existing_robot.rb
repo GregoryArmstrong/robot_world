430531bc796b9617f365f74dcc8a6354e84dd75e
GregoryArmstrong <intergalacticgentleman@gmail.com>
2015-12-03 15:58:49 -0700

    all tests passing, database migrated to sequel and sqlite3

--- test/features/user_can_delete_an_existing_robot.rb
+++ test/features/user_can_delete_an_existing_robot.rb
@@ -2,18 +2,18 @@ require_relative '../test_helper'
 
 class UserCanDeleteARobotTest < FeatureTest
 
-  # def test_user_can_delete_an_existing_robot
-  #   RobotManager.create(name: "Samus", city: "Babylon", state: "Egypt", birth_date: "1987", date_hired: "2015", department: "Avengers")
-  #   visit '/robots'
-  #
-  #   within('#robot_table') do
-  #     assert page.has_content?("Samus")
-  #   end
-  #
-  #   click_button('Delete')
-  #   within('#robot_table') do
-  #     refute page.has_content?("Samus")
-  #   end
-  # end
+  def test_user_can_delete_an_existing_robot
+    RobotManager.create(name: "Samus", city: "Babylon", state: "Egypt", birth_date: "1987", date_hired: "2015", department: "Avengers")
+    visit '/robots'
+
+    within('#robot_table') do
+      assert page.has_content?("Samus")
+    end
+
+    click_button('Delete')
+    within('#robot_table') do
+      refute page.has_content?("Samus")
+    end
+  end
 
 end
--- test/features/user_sees_greeting_on_homepage_test.rb
+++ test/features/user_sees_greeting_on_homepage_test.rb
@@ -1,13 +1,13 @@
 require_relative '../test_helper'
 
 class UserSeesGreetingOnHomepageTest < FeatureTest
-  # 
-  # def test_greeting_is_displayed
-  #   visit '/'
-  #
-  #   within('#greeting') do
-  #     assert page.has_content?("Welcome to Robo-Manager!")
-  #   end
-  # end
+
+  def test_greeting_is_displayed
+    visit '/'
+
+    within('#greeting') do
+      assert page.has_content?("Welcome to Robo-Manager!")
+    end
+  end
 
 end
