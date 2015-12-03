81410803e931e095bacb7a7558427a096920e2fb
GregoryArmstrong <intergalacticgentleman@gmail.com>
2015-12-02 17:41:39 -0700

    feature tests added, all passing

---
+++ test/features/user_can_edit_an_existing_robot_with_valid_attributes.rb
@@ -0,0 +1,22 @@
+require_relative '../test_helper'
+
+class UserCanEditAnExistingRobotTest < FeatureTest
+
+  def test_user_can_edit_a_robot_with_valid_attributes
+    RobotManager.create(name: "Samus", city: "Babylon", state: "Egypt", birth_date: "1987", date_hired: "2015", department: "Avengers")
+    visit '/robots'
+
+    click_link('edit_button')
+
+    assert_equal '/robots/1/edit', current_path
+
+    fill_in('robot[name]', :with => "Cornelius")
+    click_button('submit_button')
+
+    assert_equal '/robots', current_path
+    within('#robot_table') do
+      assert page.has_content?("Cornelius")
+    end
+  end
+
+end
