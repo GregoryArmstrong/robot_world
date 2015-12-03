22310551c82a5abb39641df5e624d37735e76600
GregoryArmstrong <intergalacticgentleman@gmail.com>
2015-12-02 17:30:32 -0700

    added feature tests for adding robot and seeing greeting on dashboard

---
+++ test/features/user_can_create_a_robot.rb
@@ -0,0 +1,22 @@
+require_relative '../test_helper'
+
+class UserCanCreateARobotTest < FeatureTest
+
+  def test_user_can_create_a_robot_with_valid_attributes
+    visit '/robots/new'
+
+    fill_in('robot[name]', :with => "Samus")
+    fill_in('robot[city]', :with => "Babylon")
+    fill_in('robot[state]', :with => "Egypt")
+    fill_in('robot[birth_date]', :with => "1987")
+    fill_in('robot[date_hired]', :with => "2015")
+    fill_in('robot[department]', :with => "Avengers")
+    click_button('submit_button')
+
+    assert_equal '/robots', current_path
+    within('#robot_table') do
+      assert page.has_content?("Samus")
+    end
+  end
+
+end
