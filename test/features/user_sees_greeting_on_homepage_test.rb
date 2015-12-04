88990eb3561257cd5c194119f9f188c8929030ce
GregoryArmstrong <intergalacticgentleman@gmail.com>
2015-12-03 17:26:58 -0700

    all feature tests passing

--- test/features/user_sees_greeting_on_homepage_test.rb
+++ test/features/user_sees_greeting_on_homepage_test.rb
@@ -3,6 +3,10 @@ require_relative '../test_helper'
 class UserSeesGreetingOnHomepageTest < FeatureTest
 
   def test_greeting_is_displayed
+    RobotManager.create(name: "Samus", city: "Babylon", state: "Egypt", birth_date: "1987", date_hired: "2015", department: "Avengers")
+    RobotManager.create(name: "Samus2", city: "Babylon", state: "Egypt", birth_date: "1987", date_hired: "2015", department: "Avengers")
+
+
     visit '/'
 
     within('#greeting') do
